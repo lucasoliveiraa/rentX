f2ec2a092661078b1aa7c17d88aa1da51f86c3a9
Lucas Oliveira <lucas.oliveira@ciss.com.br>
2021-11-13 00:22:21 -0300

    🔨   refactor/add-type-loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { RectButtonProps } from 'react-native-gesture-handler';
+import { useTheme } from 'styled-components';
 
 import {
   Container,
@@ -9,12 +11,30 @@ import {
 interface Props extends RectButtonProps {
   title: string;
   color?: string;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ title, color, ...rest }: Props) {
+export function Button({ 
+  title, 
+  color, 
+  enabled = true ,
+  loading = false ,
+  ...rest
+}: Props) {
+  const theme = useTheme();
   return (
-    <Container {...rest} color={color}>
-      <Title>{title}</Title>
+    <Container 
+      {...rest} 
+      color={color}
+      enabled={enabled}
+      loading={loading}
+    >
+      { 
+        loading 
+        ? <ActivityIndicator color={theme.colors.shape} />
+        : <Title>{title}</Title>
+      }
     </Container>
   )
 }
