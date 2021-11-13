b9bef84879e176ddce5aec7c08f6c09e4c36b10b
Lucas Oliveira <lucas.oliveira@ciss.com.br>
2021-11-13 00:24:33 -0300

    ✨  feature/screen-my-cars

---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { FlatList, StatusBar } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+import { CarDTO } from '../../dtos/CarDTO';
+
+import { Car } from '../../components/Car';
+import { Load } from '../../components/Load';
+import { BackButton } from '../../components/BackButton';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
+} from './styles';
+
+type NavigationProps = {
+  navigate: (screen: string, {}) => void;
+  goBack: () => void;
+}
+
+interface CarProps {
+  id: string;
+  car: CarDTO;
+  user_id: string;
+  startDate: string,
+  endDate: string;
+}
+
+export function MyCars() {
+  const [ cars, setCars ] = useState<CarProps[]>([]);
+  const [ loading, setLoading ] = useState(true);
+
+  const theme = useTheme();
+  const navigation = useNavigation<NavigationProps>();
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('/schedules_byuser?user_id=1&?sortby=id&order=desc');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <StatusBar 
+        barStyle='light-content' 
+        backgroundColor='transparent' 
+        translucent 
+      />
+      <Header>
+        <BackButton 
+        color={theme.colors.shape}
+        onPress={handleBack} 
+        />
+        <Title>
+          Escolha uma {'\n'} 
+          data de início e {'\n'}
+          fim do aluguel {'\n'}         
+        </Title>
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+
+      { loading ? <Load /> :
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+
+          <FlatList 
+            data={cars}
+            keyExtractor={item => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item}) => (
+              <CarWapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>Período</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign 
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWapper>
+            )}
+          />
+        </Content>
+      }
+    </Container>
+  );
+}
