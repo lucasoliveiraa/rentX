849e3b93ba7dda6040011b6fc7ea10c3516c1a58
Lucas Oliveira <lucasjackcosta18@gmail.com>
2021-11-16 23:21:41 -0300

    ✨  feature/add-splash-screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+import BrandSvg from "../../assets/brand.svg";
+import LogoSvg from "../../assets/logo.svg";
+
+import { Container } from "./styles";
+
+type NavigationProps = {
+  navigate: (screen: string) => void;
+};
+
+export function Splash() {
+  const splashAnimation = useSharedValue(0);
+  const navigation = useNavigation<NavigationProps>();
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [1, 0.3, 0],
+        Extrapolate.CLAMP
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 25, 50],
+            [0, -25, -50],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [0, 0.3, 1],
+        Extrapolate.CLAMP
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 25, 50],
+            [-50, -25, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  function startApp() {
+    navigation.navigate("Home");
+  }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 3000 }, () => {
+      "worklet";
+      runOnJS(startApp)();
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Animated.View style={[brandStyle, { position: "absolute" }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle, { position: "absolute" }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
+    </Container>
+  );
+}
