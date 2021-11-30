db9ef567db1ee4d920c14a909c24d5576ad71ef8
Lucas Oliveira <lucasjackcosta18@gmail.com>
2021-11-29 21:18:38 -0300

    ✨ chore/add-component-button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { RectButtonProps } from 'react-native-gesture-handler';
-import { useTheme } from 'styled-components';
+import React from "react";
+import { ActivityIndicator } from "react-native";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { useTheme } from "styled-components";
 
-import {
-  Container,
-  Title,
-} from './styles';
+import { Container, Title } from "./styles";
 
 interface Props extends RectButtonProps {
+  onPress: () => void;
   title: string;
   color?: string;
   enabled?: boolean;
   loading?: boolean;
+  light?: boolean;
 }
 
-export function Button({ 
-  title, 
-  color, 
-  enabled = true ,
-  loading = false ,
+export function Button({
+  onPress,
+  title,
+  color,
+  enabled = true,
+  loading = false,
+  light = false,
   ...rest
 }: Props) {
   const theme = useTheme();
   return (
-    <Container 
-      {...rest} 
+    <Container
+      {...rest}
+      onPress={onPress}
       color={color}
       enabled={enabled}
       loading={loading}
     >
-      { 
-        loading 
-        ? <ActivityIndicator color={theme.colors.shape} />
-        : <Title>{title}</Title>
-      }
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title light={light}>{title}</Title>
+      )}
     </Container>
-  )
+  );
 }
