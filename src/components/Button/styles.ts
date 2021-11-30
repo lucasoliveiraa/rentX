789bcdf0703a5a7287332f2873108b70361987f7
Lucas Oliveira <lucasjackcosta18@gmail.com>
2021-11-29 21:20:07 -0300

    💄 styles-button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,6 @@
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
-import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native';
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
 interface ButtonProps extends RectButtonProps {
   color?: string;
@@ -8,21 +8,24 @@ interface ButtonProps extends RectButtonProps {
   loading?: boolean;
 }
 
+interface ButtonTextProps {
+  light: boolean;
+}
+
 export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
   padding: 19px;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme, color }) => 
-    color ? color : theme.colors.main
-  };
-  opacity: ${({ enabled, loading }) => 
-    enabled === false || loading === true ? 0.5 : 1 
-  };
+  background-color: ${({ theme, color }) =>
+    color ? color : theme.colors.main};
+  opacity: ${({ enabled, loading }) =>
+    enabled === false || loading === true ? 0.5 : 1};
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonTextProps>`
   font-family: ${({ theme }) => theme.fonts.primary_500};
   font-size: ${RFValue(15)}px;
-  color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme, light }) =>
+    light ? theme.colors.header : theme.colors.shape};
 `;
