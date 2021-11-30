974b994196ffabd79d3d55ed6eede75deb1f2cb6
Lucas Oliveira <lucasjackcosta18@gmail.com>
2021-11-29 21:17:53 -0300

    ✨ feature/add-login-screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
+import theme from "../../styles/theme";
+
+import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
+
+export function SignIn() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Title>Estamos{"\n"}quase lá.</Title>
+        <SubTitle>
+          Faça seu login para começar{"\n"}uma experiencia incrivel.
+        </SubTitle>
+      </Header>
+
+      <Form>
+        <Input
+          iconName="mail"
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <PasswordInput iconName="lock" placeholder="Senha" />
+      </Form>
+
+      <Footer>
+        <Button
+          title="Login"
+          onPress={() => {}}
+          enabled={true}
+          loading={false}
+        />
+        <Button
+          title="Criar conta gratuita"
+          onPress={() => {}}
+          color={theme.colors.background_secondary}
+          light
+          enabled={false}
+          loading={false}
+          style={{ marginTop: 8 }}
+        />
+      </Footer>
+    </Container>
+  );
+}
