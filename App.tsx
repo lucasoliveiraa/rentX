c05e4ce530ec0c5d6c3660e257a134dce2791dee
Lucas Oliveira <lucas.oliveira@ciss.com.br>
2021-12-22 23:06:05 -0300

    ✨  chore/add-hooks

--- App.tsx
+++ App.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
 import {
   useFonts,
   Inter_400Regular,
-  Inter_500Medium
-} from '@expo-google-fonts/inter';
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 import {
   Archivo_400Regular,
   Archivo_500Medium,
-  Archivo_600SemiBold
-} from '@expo-google-fonts/archivo';
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { AppProvider } from "./src/hooks";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import theme from './src/styles/theme';
+import theme from "./src/styles/theme";
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Archivo_400Regular,
     Archivo_500Medium,
-    Archivo_600SemiBold
+    Archivo_600SemiBold,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </ThemeProvider>
   );
 }
