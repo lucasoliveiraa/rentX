4db1ba654529b13e8d3ff3551042646f3a41c6f6
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-03-23 23:09:04 -0300

    Confirmação de sign out

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -99,6 +99,23 @@ export function Profile() {
     }
   }
 
+  async function handleSignOut() {
+    Alert.alert(
+      "Tem certeza?",
+      "Se você sair, irá precisar de internet para conectar-se novamente",
+      [
+        {
+          text: "Cancelar",
+          onPress: () => {},
+        },
+        {
+          text: "Sair",
+          onPress: () => signOut(),
+        },
+      ]
+    );
+  }
+
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -107,7 +124,7 @@ export function Profile() {
             <HeaderTop>
               <BackButton onPress={handleBack} color={theme.colors.shape} />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={signOut}>
+              <LogoutButton onPress={handleSignOut}>
                 <Feather name="power" size={24} color={theme.colors.shape} />
               </LogoutButton>
             </HeaderTop>
@@ -169,8 +186,8 @@ export function Profile() {
             )}
             <Button
               title="Salvar alterações"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleProfileUpdate}
+              enabled={true}
             />
           </Content>
         </Container>
